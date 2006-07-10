decbb6a0c535c15ae02bc07c79d541047a5c9520
kaashoek <kaashoek>
2006-07-10 19:06:48 +0000

    queue with disk requests

--- param.h
+++ param.h
@@ -2,5 +2,6 @@
 #define PAGE 4096
 #define KSTACKSIZE PAGE
 #define NCPU 8
-#define NOFILE 16 // file descriptors per process
-#define NFD 100   // file descriptors per system
+#define NOFILE 16    // file descriptors per process
+#define NFD 100      // file descriptors per system
+#define NREQUEST 100 // outstanding disk requests
