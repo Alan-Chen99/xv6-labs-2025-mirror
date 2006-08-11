7234e2fde8906a67b7635baca7c2194dcc4f14d7
rtm <rtm>
2006-08-11 13:55:18 +0000

    init creates console, opens 0/1/2, runs sh sh accepts 0-argument commands (like userfs) reads from console

--- console.c
+++ console.c
@@ -158,14 +158,13 @@ void panic(char *s) {
     ;
 }
 
-int console_write(int minor, void *buf, int n) {
+int console_write(int minor, char *buf, int n) {
   int i;
-  uchar *b = buf;
 
   acquire(&console_lock);
 
   for (i = 0; i < n; i++) {
-    cons_putc((int)b[i]);
+    cons_putc(buf[i] & 0xff);
   }
 
   release(&console_lock);
@@ -529,6 +528,7 @@ void kbd_intr() {
     kbd_buf[kbd_w++] = c;
     if (kbd_w >= KBD_BUF)
       kbd_w = 0;
+    wakeup(&kbd_r);
   } else {
     cprintf("kbd overflow\n");
   }
@@ -536,11 +536,35 @@ void kbd_intr() {
   release(&kbd_lock);
 }
 
+int console_read(int minor, char *dst, int n) {
+  uint target = n;
+
+  acquire(&kbd_lock);
+
+  while (kbd_w == kbd_r)
+    sleep(&kbd_r, &kbd_lock);
+
+  while (n > 0 && kbd_w != kbd_r) {
+    *dst = kbd_buf[kbd_r];
+    cons_putc(*dst & 0xff);
+    dst++;
+    --n;
+    kbd_r++;
+    if (kbd_r >= KBD_BUF)
+      kbd_r = 0;
+  }
+
+  release(&kbd_lock);
+
+  return target - n;
+}
+
 void console_init() {
   initlock(&console_lock, "console");
   initlock(&kbd_lock, "kbd");
 
   devsw[CONSOLE].d_write = console_write;
+  devsw[CONSOLE].d_read = console_read;
 
   ioapic_enable(IRQ_KBD, 1);
 
--- fs.c
+++ fs.c
@@ -91,6 +91,7 @@ loop:
         goto loop;
       }
       ip->count++;
+      ip->busy = 1;
       release(&inode_table_lock);
       return ip;
     }
@@ -242,15 +243,14 @@ uint bmap(struct inode *ip, uint bn) {
 
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
-int readi(struct inode *ip, void *xdst, uint off, uint n) {
-  char *dst = (char *)xdst;
+int readi(struct inode *ip, char *dst, uint off, uint n) {
   uint target = n, n1;
   struct buf *bp;
 
   if (ip->type == T_DEV) {
     if (ip->major < 0 || ip->major >= NDEV || !devsw[ip->major].d_read)
       return -1;
-    return devsw[ip->major].d_read(ip->minor, xdst, n);
+    return devsw[ip->major].d_read(ip->minor, dst, n);
   }
 
   while (n > 0 && off < ip->size) {
@@ -269,7 +269,7 @@ int readi(struct inode *ip, void *xdst, uint off, uint n) {
 
 #define MIN(a, b) ((a < b) ? a : b)
 
-int writei(struct inode *ip, void *addr, uint off, uint n) {
+int writei(struct inode *ip, char *addr, uint off, uint n) {
   if (ip->type == T_DEV) {
     if (ip->major < 0 || ip->major >= NDEV || !devsw[ip->major].d_write)
       return -1;
@@ -373,7 +373,7 @@ struct inode *mknod(struct inode *dp, char *cp, short type, short major,
   int i;
   struct buf *bp = 0;
 
-  cprintf("mknod: %s %d %d %d\n", cp, type, major, minor);
+  cprintf("mknod: dir %d %s %d %d %d\n", dp->inum, cp, type, major, minor);
 
   ip = ialloc(dp->dev, type);
   if (ip == 0)
