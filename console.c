51611ae5c06e3b07107a62d020e9b31d90300fa5
kaashoek <kaashoek>
2006-09-03 17:34:09 +0000

    don't send keyboard interrupts to the second cpu, if we have only one cpu. xv6 seems to work fine on uniprocessor now.

--- console.c
+++ console.c
@@ -543,8 +543,9 @@ int console_read(int minor, char *dst, int n) {
 
   acquire(&kbd_lock);
 
-  while (kbd_w == kbd_r)
+  while (kbd_w == kbd_r) {
     sleep(&kbd_r, &kbd_lock);
+  }
 
   while (n > 0 && kbd_w != kbd_r) {
     *dst = (kbd_buf[kbd_r]) & 0xff;
@@ -568,7 +569,7 @@ void console_init() {
   devsw[CONSOLE].d_write = console_write;
   devsw[CONSOLE].d_read = console_read;
 
-  ioapic_enable(IRQ_KBD, 1);
+  ioapic_enable(IRQ_KBD, 0);
 
   use_console_lock = 1;
 }
