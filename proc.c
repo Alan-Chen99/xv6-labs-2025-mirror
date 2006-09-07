8405762aba8f19dec4597dfa9134cf748f11bdc5
rsc <rsc>
2006-09-07 15:45:38 +0000

    debugging prints

--- proc.c
+++ proc.c
@@ -370,3 +370,18 @@ int proc_wait(void) {
     sleep(cp, &proc_table_lock);
   }
 }
+
+// Print a process listing to console.  For debugging.
+// Runs when user types ^P on console.
+// No lock to avoid wedging a stuck machine further.
+void procdump(void) {
+  int i;
+  struct proc *p;
+
+  for (i = 0; i < NPROC; i++) {
+    p = &proc[i];
+    if (p->state == UNUSED)
+      continue;
+    cprintf("%d %d %p\n", p->pid, p->state);
+  }
+}
