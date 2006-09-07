052b64054413e5e8bb3719ec38834c30e6366068
rsc <rsc>
2006-09-07 01:56:22 +0000

    more defensive programming

--- proc.c
+++ proc.c
@@ -339,7 +339,9 @@ int proc_wait(void) {
     havekids = 0;
     for (i = 0; i < NPROC; i++) {
       p = &proc[i];
-      if (p->state != UNUSED && p->ppid == cp->pid) {
+      if (p->state == UNUSED)
+        continue;
+      if (p->ppid == cp->pid) {
         if (p->state == ZOMBIE) {
           // Found one.
           kfree(p->mem, p->sz);
@@ -347,6 +349,7 @@ int proc_wait(void) {
           pid = p->pid;
           p->state = UNUSED;
           p->pid = 0;
+          p->ppid = 0;
           release(&proc_table_lock);
           return pid;
         }
