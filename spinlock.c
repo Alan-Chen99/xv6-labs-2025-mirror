ababd0ce97db0ee76d89dfc0db5dd50dffe52fa5
rsc <rsc>
2006-09-07 16:53:49 +0000

    comment memory barriers

--- spinlock.c
+++ spinlock.c
@@ -44,6 +44,10 @@ void acquire(struct spinlock *lock) {
 
   while (cmpxchg(0, 1, &lock->locked) == 1)
     ;
+
+  // Now that lock is acquired, make sure
+  // we wait for all pending writes from other
+  // processors.
   cpuid(0, 0, 0, 0, 0); // memory barrier
 
   // Record info about lock acquisition for debugging.
@@ -56,13 +60,16 @@ void acquire(struct spinlock *lock) {
 
 // Release the lock.
 void release(struct spinlock *lock) {
-
   if (!holding(lock))
     panic("release");
 
   lock->pcs[0] = 0;
   lock->cpu = 0xffffffff;
+
+  // Before unlocking the lock, make sure to flush
+  // any pending memory writes from this processor.
   cpuid(0, 0, 0, 0, 0); // memory barrier
+
   lock->locked = 0;
   if (--cpus[cpu()].nlock == 0)
     sti();
