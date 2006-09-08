13332f8a3a6d8c945ef4d3372a48bea3194d7187
rsc <rsc>
2006-09-08 15:18:58 +0000

    make lines shorter

--- spinlock.c
+++ spinlock.c
@@ -47,7 +47,7 @@ void acquire(struct spinlock *lock) {
 
   // Serialize instructions: now that lock is acquired, make sure
   // we wait for all pending writes from other processors.
-  cpuid(0, 0, 0, 0, 0); // memory barrier (see Ch 7 of IA-32 manual, vol 3)
+  cpuid(0, 0, 0, 0, 0); // memory barrier (see Ch 7, IA-32 manual vol 3)
 
   // Record info about lock acquisition for debugging.
   // The +10 is only so that we can tell the difference
@@ -67,7 +67,7 @@ void release(struct spinlock *lock) {
 
   // Serialize instructions: before unlocking the lock, make sure
   // to flush any pending memory writes from this processor.
-  cpuid(0, 0, 0, 0, 0); // memory barrier (see Ch 7 of IA-32 manual, vol 3)
+  cpuid(0, 0, 0, 0, 0); // memory barrier (see Ch 7, IA-32 manual vol 3)
 
   lock->locked = 0;
   if (--cpus[cpu()].nlock == 0)
