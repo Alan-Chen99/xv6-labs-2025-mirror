9f18f682aa29a509be25dba901f52d6ec5146941
rsc <rsc>
2007-08-08 08:04:20 +0000

    change gets to return \n, to distinguish 0-byte read from empty line

--- ulib.c
+++ ulib.c
@@ -51,9 +51,9 @@ char *gets(char *buf, int max) {
     cc = read(0, &c, 1);
     if (cc < 1)
       break;
+    buf[i++] = c;
     if (c == '\n' || c == '\r')
       break;
-    buf[i++] = c;
   }
   buf[i] = '\0';
   return buf;
