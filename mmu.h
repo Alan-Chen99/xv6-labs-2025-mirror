05abb8987216578703cfa081d83c13491b078d5a
rsc <rsc>
2007-08-08 09:02:42 +0000

    add DPL_USER constant

--- mmu.h
+++ mmu.h
@@ -75,6 +75,8 @@ struct segdesc {
                    0,                                                          \
                    (uint)(base) >> 24}
 
+#define DPL_USER 0x3 // User DPL
+
 // Application segment type bits
 #define STA_X 0x8 // Executable segment
 #define STA_E 0x4 // Expand down (non-executable segments)
