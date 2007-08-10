db4c840242df7cbe41f91faa7245e319aba675b7
rsc <rsc>
2007-08-10 17:45:49 +0000

    nit

--- spinlock.c
+++ spinlock.c
@@ -34,8 +34,8 @@ void getcallerpcs(void *v, uint pcs[]) {
 
 // Acquire the lock.
 // Loops (spins) until the lock is acquired.
-// (Because contention is handled by spinning, must not
-// go to sleep holding any locks.)
+// (Because contention is handled by spinning,
+// must not go to sleep holding any locks.)
 void acquire(struct spinlock *lock) {
   if (holding(lock))
     panic("acquire");
