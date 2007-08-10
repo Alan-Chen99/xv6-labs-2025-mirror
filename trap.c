30e8e987372f3653e596b08842a3353cf3c1ef4a
rsc <rsc>
2007-08-10 17:19:15 +0000

    add note

--- trap.c
+++ trap.c
@@ -49,6 +49,7 @@ void trap(struct trapframe *tf) {
         proc_exit();
 
       // Force process to give up CPU and let others run.
+      // If locks were held with interrupts on, would need to check nlock.
       if (cp->state == RUNNING)
         yield();
     }
