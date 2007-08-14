419bf3c3ec576aa515b658faa9a798b56cfc3bf3
rsc <rsc>
2007-08-14 18:46:29 +0000

    formatting, sleep bug fix

--- ide.c
+++ ide.c
@@ -29,6 +29,7 @@ struct ide_request {
   void *addr;
   uint nsecs;
   uint read;
+  int done;
 };
 
 static struct ide_request request[NREQUEST];
@@ -40,7 +41,8 @@ static int disk_queue;
 
 static int ide_probe_disk1(void);
 
-// Wait for IDE disk to become ready.
+// PAGEBREAK: 10
+//  Wait for IDE disk to become ready.
 static int ide_wait_ready(int check_error) {
   int r;
 
@@ -83,6 +85,7 @@ static int ide_probe_disk1(void) {
 // Interrupt handler - wake up the request that just finished.
 void ide_intr(void) {
   acquire(&ide_lock);
+  request[tail].done = 1;
   wakeup(&request[tail]);
   release(&ide_lock);
 }
@@ -109,7 +112,8 @@ static void ide_start_request(void) {
   }
 }
 
-// Run an entire disk operation.
+// PAGEBREAK: 30
+//  Run an entire disk operation.
 void ide_rw(int diskno, uint secno, void *addr, uint nsecs, int read) {
   struct ide_request *r;
 
@@ -128,13 +132,15 @@ void ide_rw(int diskno, uint secno, void *addr, uint nsecs, int read) {
   r->nsecs = nsecs;
   r->diskno = diskno;
   r->read = read;
+  r->done = 0;
   head = (head + 1) % NREQUEST;
 
   // Start request if necessary.
   ide_start_request();
 
   // Wait for request to finish.
-  sleep(r, &ide_lock);
+  while (!r->done)
+    sleep(r, &ide_lock);
 
   // Finish request.
   if (read) {
