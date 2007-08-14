cac65d9c311c0cf37061520155b81b51719fc0b2
rsc <rsc>
2007-08-14 19:42:14 +0000

    cleanup loop

--- bio.c
+++ bio.c
@@ -67,19 +67,18 @@ static struct buf *bget(uint dev, uint sector) {
 
 loop:
   // Try for cached block.
-  for (b = bufhead.next; b != &bufhead; b = b->next)
-    if ((b->flags & (B_BUSY | B_VALID)) && b->dev == dev && b->sector == sector)
-      break;
-
-  if (b != &bufhead) {
-    if (b->flags & B_BUSY) {
-      sleep(buf, &buf_table_lock);
-      goto loop;
+  for (b = bufhead.next; b != &bufhead; b = b->next) {
+    if ((b->flags & (B_BUSY | B_VALID)) && b->dev == dev &&
+        b->sector == sector) {
+      if (b->flags & B_BUSY) {
+        sleep(buf, &buf_table_lock);
+        goto loop;
+      }
+      b->flags |= B_BUSY;
+      // b->flags &= ~B_VALID; // Force reread from disk
+      release(&buf_table_lock);
+      return b;
     }
-    b->flags |= B_BUSY;
-    // b->flags &= ~B_VALID; // Force reread from disk
-    release(&buf_table_lock);
-    return b;
   }
 
   // Allocate fresh block.
