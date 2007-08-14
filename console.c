e52927e954ab353743f2966bacb69de1206afe9c
rsc <rsc>
2007-08-14 19:41:01 +0000

    fixes

--- console.c
+++ console.c
@@ -188,7 +188,7 @@ void kbd_intr(void) {
   static uchar *charcode[4] = {normalmap, shiftmap, ctlmap, ctlmap};
   uint st, data, c;
 
-  acquire(&kbd_lock);
+  acquire(&kbd.lock);
 
   st = inb(KBSTATP);
   if ((st & KBS_DIB) == 0)
@@ -234,17 +234,17 @@ void kbd_intr(void) {
     break;
 
   default:
-    if (((kbd_w + 1) % KBD_BUF) != kbd_r) {
-      kbd_buf[kbd_w++] = c;
-      if (kbd_w >= KBD_BUF)
-        kbd_w = 0;
-      wakeup(&kbd_r);
+    if (((kbd.w + 1) % KBD_BUF) != kbd.r) {
+      kbd.buf[kbd.w++] = c;
+      if (kbd.w >= KBD_BUF)
+        kbd.w = 0;
+      wakeup(&kbd.r);
     }
     break;
   }
 
 out:
-  release(&kbd_lock);
+  release(&kbd.lock);
 }
 
 // PAGEBREAK: 25
