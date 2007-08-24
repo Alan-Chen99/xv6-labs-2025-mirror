97677569ecc3252a38807db8dd37024d34ff5b9d
rsc <rsc>
2007-08-24 20:20:23 +0000

    test that fork fails gracefully

--- usertests.c
+++ usertests.c
@@ -1143,6 +1143,42 @@ void iref(void) {
   printf(1, "empty file name OK\n");
 }
 
+// test that fork fails gracefully
+// the forktest binary also does this, but it runs out of proc entries first.
+// inside the bigger usertests binary, we run out of memory first.
+void forktest(void) {
+  int n, pid;
+
+  printf(1, "fork test\n");
+
+  for (n = 0; n < 1000; n++) {
+    pid = fork();
+    if (pid < 0)
+      break;
+    if (pid == 0)
+      exit();
+  }
+
+  if (n == 1000) {
+    printf(1, "fork claimed to work 1000 times!\n");
+    exit();
+  }
+
+  for (; n > 0; n--) {
+    if (wait() < 0) {
+      printf(1, "wait stopped early\n");
+      exit();
+    }
+  }
+
+  if (wait() != -1) {
+    printf(1, "wait got too many\n");
+    exit();
+  }
+
+  printf(1, "fork test OK\n");
+}
+
 int main(int argc, char *argv[]) {
   printf(1, "usertests starting\n");
 
@@ -1175,6 +1211,7 @@ int main(int argc, char *argv[]) {
   sharedfd();
   dirfile();
   iref();
+  forktest();
 
   exectest();
 
