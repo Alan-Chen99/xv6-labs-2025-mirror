c98a4f8b909bfadb3202ca694b914587b6161465
rsc <rsc>
2007-08-30 18:20:53 +0000

    oops - broke circular buffer

--- console.c
+++ console.c
@@ -205,7 +205,7 @@ void console_intr(int (*getc)(void)) {
       break;
     default:
       if (c != 0 && input.e < input.r + INPUT_BUF) {
-        input.buf[input.e++] = c;
+        input.buf[input.e++ % INPUT_BUF] = c;
         cons_putc(c);
         if (c == '\n' || c == C('D') || input.e == input.r + INPUT_BUF) {
           input.w = input.e;
@@ -234,7 +234,7 @@ int console_read(struct inode *ip, char *dst, int n) {
       }
       sleep(&input.r, &input.lock);
     }
-    c = input.buf[input.r++];
+    c = input.buf[input.r++ % INPUT_BUF];
     if (c == C('D')) { // EOF
       if (n < target) {
         // Save ^D for next time, to make sure
@@ -247,8 +247,6 @@ int console_read(struct inode *ip, char *dst, int n) {
     --n;
     if (c == '\n')
       break;
-    if (input.r >= INPUT_BUF)
-      input.r = 0;
   }
   release(&input.lock);
   ilock(ip);
