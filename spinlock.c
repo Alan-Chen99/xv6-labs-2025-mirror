182ee36e0ddd73a950b95fe08d3ca8dab53adb27
rtm <rtm>
2007-08-31 19:55:27 +0000

    continuous quality management

--- spinlock.c
+++ spinlock.c
@@ -18,8 +18,8 @@ void initlock(struct spinlock *lock, char *name) {
 
 // Acquire the lock.
 // Loops (spins) until the lock is acquired.
-// (Because contention is handled by spinning,
-// must not go to sleep holding any locks.)
+// Holding a lock for a long time may cause
+// other CPUs to waste time spinning to acquire it.
 void acquire(struct spinlock *lock) {
   if (holding(lock))
     panic("acquire");
