58f29df502c7dd02b9a0d5949a738192686c6045
rsc <rsc>
2009-03-08 21:27:57 +0000

    xv6/x86.h: add stosb, fix bugs in insl/outsl (rep not repne)

--- x86.h
+++ x86.h
@@ -8,7 +8,7 @@ static inline uchar inb(ushort port) {
 }
 
 static inline void insl(int port, void *addr, int cnt) {
-  asm volatile("cld; repne insl"
+  asm volatile("cld; rep insl"
                : "=D"(addr), "=c"(cnt)
                : "d"(port), "0"(addr), "1"(cnt)
                : "memory", "cc");
@@ -23,10 +23,17 @@ static inline void outw(ushort port, ushort data) {
 }
 
 static inline void outsl(int port, const void *addr, int cnt) {
-  asm volatile("cld; repne outsl"
+  asm volatile("cld; rep outsl"
                : "=S"(addr), "=c"(cnt)
                : "d"(port), "0"(addr), "1"(cnt)
                : "cc");
+}
+
+static inline void stosb(void *addr, int data, int cnt) {
+  asm volatile("cld; rep stosb"
+               : "=D"(addr), "=c"(cnt)
+               : "0"(addr), "1"(cnt), "a"(data)
+               : "memory", "cc");
 }
 
 static inline uint read_ebp(void) {
