bcd1cebf996cc0589cdf1efdc90659450bc348eb
Robert Morris <rtm@nephron.lcs.mit.edu>
2010-08-05 16:00:59 -0400

    remove some unused vm #defines fix corner cases with alignment when mapping kernel ELF file

--- proc.c
+++ proc.c
@@ -373,9 +373,9 @@ int wait(void) {
         // Found one.
         pid = p->pid;
         kfree(p->kstack, KSTACKSIZE);
+        p->kstack = 0;
         freevm(p->pgdir);
         p->state = UNUSED;
-        p->kstack = 0;
         p->pid = 0;
         p->parent = 0;
         p->name[0] = 0;
