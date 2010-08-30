42ac27f516f2a61972799bc9b38cb147d3d9c223
Robert Morris <rtm@nephron.lcs.mit.edu>
2010-08-30 06:38:58 -0400

    set only PG and WP in vminit; the rest don't seem to be needed and are confusing

--- main.c
+++ main.c
@@ -35,7 +35,7 @@ void jkstack(void) {
 
 void mainc(void) {
   cprintf("\ncpu%d: starting xv6\n\n", cpu->id);
-  kvmalloc(); // initialze the kernel page table
+  kvmalloc(); // initialize the kernel page table
   pinit();    // process table
   tvinit();   // trap vectors
   binit();    // buffer cache
