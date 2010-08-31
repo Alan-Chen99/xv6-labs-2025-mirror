595e534d2c8ce122a8b9f06f4749aee691e926d8
Austin Clements <amdragon@mit.edu>
2010-08-31 17:52:03 -0400

    Page break kalloc.c

--- kalloc.c
+++ kalloc.c
@@ -28,10 +28,11 @@ void kinit(void) {
     kfree(p1);
 }
 
-// Free the page of physical memory pointed at by v,
-// which normally should have been returned by a
-// call to kalloc().  (The exception is when
-// initializing the allocator; see kinit above.)
+// PAGEBREAK: 21
+//  Free the page of physical memory pointed at by v,
+//  which normally should have been returned by a
+//  call to kalloc().  (The exception is when
+//  initializing the allocator; see kinit above.)
 void kfree(char *v) {
   struct run *r;
 
