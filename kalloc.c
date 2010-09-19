8f16a680544f97d1f1a05aafaa08e969e9ee3193
Robert Morris <rtm@csail.mit.edu>
2010-09-19 07:18:42 -0400

    exec questions

--- kalloc.c
+++ kalloc.c
@@ -17,10 +17,10 @@ struct {
   struct run *freelist;
 } kmem;
 
+extern char end[]; // first address after kernel loaded from ELF file
+
 // Initialize free list of physical pages.
 void kinit(void) {
-  extern char end[];
-
   initlock(&kmem.lock, "kmem");
   char *p = (char *)PGROUNDUP((uint)end);
   for (; p + PGSIZE - 1 < (char *)PHYSTOP; p += PGSIZE)
@@ -35,7 +35,7 @@ void kinit(void) {
 void kfree(char *v) {
   struct run *r;
 
-  if (((uint)v) % PGSIZE || (uint)v < 1024 * 1024 || (uint)v >= PHYSTOP)
+  if (((uint)v) % PGSIZE || v < end || (uint)v >= PHYSTOP)
     panic("kfree");
 
   // Fill with junk to catch dangling refs.
