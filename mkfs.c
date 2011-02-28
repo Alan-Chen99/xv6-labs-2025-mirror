0169f0276cc95a34d9a5dcc518aee2f4880f4601
Russ Cox <rsc@swtch.com>
2011-02-28 09:32:20 -0500

    fixes from Peter Froehlich

--- mkfs.c
+++ mkfs.c
@@ -207,7 +207,7 @@ void balloc(int used) {
   int i;
 
   printf("balloc: first %d blocks have been allocated\n", used);
-  assert(used < 512);
+  assert(used < 512 * 8);
   bzero(buf, 512);
   for (i = 0; i < used; i++) {
     buf[i / 8] = buf[i / 8] | (0x1 << (i % 8));
