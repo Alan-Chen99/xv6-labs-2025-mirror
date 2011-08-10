ad23f0447fcb6d726a11ba8bf9ffd8d0b0867510
Frans Kaashoek <kaashoek@225.sub-75-213-134.myvzw.com>
2011-08-09 23:22:48 -0400

    One more low address

--- lapic.c
+++ lapic.c
@@ -3,6 +3,7 @@
 
 #include "types.h"
 #include "defs.h"
+#include "memlayout.h"
 #include "traps.h"
 #include "mmu.h"
 #include "x86.h"
@@ -123,7 +124,10 @@ void microdelay(int us) {}
 
 // Start additional processor running bootstrap code at addr.
 // See Appendix B of MultiProcessor Specification.
-void lapicstartap(uchar apicid, uint addr) {
+void
+
+lapicstartap(uchar apicid, uint addr)
+{
   int i;
   ushort *wrv;
 
@@ -132,7 +136,7 @@ void lapicstartap(uchar apicid, uint addr) {
   // the AP startup code prior to the [universal startup algorithm]."
   outb(IO_RTC, 0xF); // offset 0xF is shutdown code
   outb(IO_RTC + 1, 0x0A);
-  wrv = (ushort *)(0x40 << 4 | 0x67); // Warm reset vector
+  wrv = (ushort *)P2V((0x40 << 4 | 0x67)); // Warm reset vector
   wrv[0] = 0;
   wrv[1] = addr >> 4;
 
