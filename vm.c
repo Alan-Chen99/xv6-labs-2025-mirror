6eb6006190a56ba3cec275c88d51d4345e187c43
Frans Kaashoek <kaashoek@mit.edu>
2012-08-22 21:24:13 -0400

    Runoff still complaints these lines are too long, but it is now ok on printout

--- vm.c
+++ vm.c
@@ -114,7 +114,7 @@ static struct kmap {
 } kmap[] = {
     {(void *)KERNBASE, 0, EXTMEM, PTE_W},            // I/O space
     {(void *)KERNLINK, V2P(KERNLINK), V2P(data), 0}, // kernel text+rodata
-    {(void *)data, V2P(data), PHYSTOP, PTE_W},       // kernel data, memory
+    {(void *)data, V2P(data), PHYSTOP, PTE_W},       // kernel data+memory
     {(void *)DEVSPACE, DEVSPACE, 0, PTE_W},          // more devices
 };
 
