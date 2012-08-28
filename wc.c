37886fe154a3409b46065660a2d4018207e8e53f
Robert Morris <rtm@csail.mit.edu>
2012-08-28 12:57:05 -0400

    junk

--- wc.c
+++ wc.c
@@ -40,7 +40,7 @@ int main(int argc, char *argv[]) {
 
   for (i = 1; i < argc; i++) {
     if ((fd = open(argv[i], 0)) < 0) {
-      printf(1, "cat: cannot open %s\n", argv[i]);
+      printf(1, "wc: cannot open %s\n", argv[i]);
       exit();
     }
     wc(fd, argv[i]);
