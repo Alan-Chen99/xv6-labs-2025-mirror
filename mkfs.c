6642d32b26e84ab27fb9bc82019b1f5a735f70d2
Ayan Shafqat <selfjam@gmail.com>
2015-08-12 22:11:39 -0400

    Fixed broken build on Arch Linux

--- mkfs.c
+++ mkfs.c
@@ -11,12 +11,14 @@
 #include "stat.h"
 #include "param.h"
 
+#ifndef static_assert
 #define static_assert(a, b)                                                    \
   do {                                                                         \
     switch (0)                                                                 \
     case 0:                                                                    \
     case (a):;                                                                 \
   } while (0)
+#endif
 
 #define NINODES 200
 
