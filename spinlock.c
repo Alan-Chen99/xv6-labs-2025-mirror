91e3cbc60dbdaab953a6f5bd3024e5013dd3e862
Robert Morris <rtm@csail.mit.edu>
2016-09-02 05:40:54 -0400

    comment about sched() saving/restoring cpu->intena

--- spinlock.c
+++ spinlock.c
@@ -87,8 +87,9 @@ void pushcli(void) {
 
   eflags = readeflags();
   cli();
-  if (cpu->ncli++ == 0)
+  if (cpu->ncli == 0)
     cpu->intena = eflags & FL_IF;
+  cpu->ncli += 1;
 }
 
 void popcli(void) {
