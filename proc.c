5442e0f6435f72c007b198cc4a743111ced9b530
Frans Kaashoek <kaashoek@mit.edu>
2017-02-01 19:21:43 -0500

    use panic

--- proc.c
+++ proc.c
@@ -27,12 +27,8 @@ int cpuid() { return mycpu() - cpus; }
 
 // Must be called with interrupts disabled
 struct cpu *mycpu(void) {
-  if (readeflags() & FL_IF) {
-    // Would prefer to panic but panic calls mycpu().
-    cprintf("mycpu called from %x with interrupts enabled\n",
-            __builtin_return_address(0));
-  }
-
+  if (readeflags() & FL_IF)
+    panic("mycpu called with interrupts enabled\n");
   return &cpus[lapiccpunum()];
 }
 
