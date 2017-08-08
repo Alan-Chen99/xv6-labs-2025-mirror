19070d77815189fb75acee1e28a5945806bfa571
Robert Morris <rtm@csail.mit.edu>
2017-08-08 13:48:48 -0400

    protect ip->valid and ip->nlink with sleep lock in iput()

--- file.h
+++ file.h
@@ -10,11 +10,11 @@ struct file {
 
 // in-memory copy of an inode
 struct inode {
-  uint dev;  // Device number
-  uint inum; // Inode number
-  int ref;   // Reference count
-  struct sleeplock lock;
-  int valid; // remainder has been read from disk?
+  uint dev;              // Device number
+  uint inum;             // Inode number
+  int ref;               // Reference count
+  struct sleeplock lock; // protects everything below here
+  int valid;             // inode has been read from disk?
 
   short type; // copy of disk inode
   short major;
