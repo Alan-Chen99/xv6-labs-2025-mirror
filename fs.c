11260a69c0963538c2d5603adfd1aeb2daafbf0e
Robert Morris <rtm@csail.mit.edu>
2019-06-04 14:20:37 -0400

    timer interrupts -> supervisor software interrupt

--- fs.c
+++ fs.c
@@ -172,10 +172,6 @@ void iinit(int dev) {
   readsb(dev, &sb);
   if (sb.magic != FSMAGIC)
     panic("invalid file system");
-  printf("sb: size %d nblocks %d ninodes %d nlog %d logstart %d\
- inodestart %d bmap start %d\n",
-         sb.size, sb.nblocks, sb.ninodes, sb.nlog, sb.logstart, sb.inodestart,
-         sb.bmapstart);
 }
 
 static struct inode *iget(uint dev, uint inum);
