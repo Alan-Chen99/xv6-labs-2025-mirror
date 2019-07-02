d318c15dbb2976a6f819935a2df49a1c13b0d1e0
Robert Morris <rtm@csail.mit.edu>
2019-07-02 11:17:50 -0400

    avoid allocproc() returning a struct proc with non-zero p->sz

--- kernel/proc.c
+++ kernel/proc.c
@@ -112,9 +112,11 @@ static void freeproc(struct proc *p) {
   if (p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
+  p->sz = 0;
   p->pid = 0;
   p->parent = 0;
   p->name[0] = 0;
+  p->chan = 0;
   p->killed = 0;
   p->state = UNUSED;
 }
