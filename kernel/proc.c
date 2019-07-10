04b5000a94529ce346f3a7f1635ef099c27137cb
Robert Morris <rtm@csail.mit.edu>
2019-07-10 08:57:51 -0400

    tweak some comments.

--- kernel/proc.c
+++ kernel/proc.c
@@ -6,20 +6,16 @@
 #include "proc.h"
 #include "defs.h"
 
-struct proc proc[NPROC];
-
 struct cpu cpus[NCPU];
+
+struct proc proc[NPROC];
 
 struct proc *initproc;
 
-struct spinlock pid_lock;
 int nextpid = 1;
+struct spinlock pid_lock;
 
 extern void forkret(void);
-
-// for returning  out of the kernel
-extern void sysexit(void);
-
 static void wakeup1(struct proc *chan);
 
 extern char trampout[]; // trampoline.S
@@ -265,8 +261,8 @@ int fork(void) {
   return pid;
 }
 
-// Pass p's abandoned children to init. p and p's parent
-// are locked.
+// Pass p's abandoned children to init.
+// Caller must hold p->lock and parent->lock.
 void reparent(struct proc *p, struct proc *parent) {
   struct proc *pp;
   int child_of_init = (p->parent == initproc);
@@ -498,15 +494,15 @@ void sleep(void *chan, struct spinlock *lk) {
 
 // PAGEBREAK!
 //  Wake up p, used by exit()
-//  Caller should lock p.
+//  Caller must hold p->lock.
 static void wakeup1(struct proc *p) {
   if (p->chan == p && p->state == SLEEPING) {
     p->state = RUNNABLE;
   }
 }
 
-// Wake up all processes sleeping on chan. Never
-// called when holding a p->lock
+// Wake up all processes sleeping on chan.
+// Must be called without any p->lock.
 void wakeup(void *chan) {
   struct proc *p;
 
--- kernel/trap.c
+++ kernel/trap.c
@@ -88,7 +88,7 @@ void usertrapret(void) {
   p->tf->kernel_satp = r_satp();
   p->tf->kernel_sp = (uint64)p->kstack + PGSIZE;
   p->tf->kernel_trap = (uint64)usertrap;
-  p->tf->hartid = r_tp();
+  p->tf->kernel_hartid = r_tp();
 
   // set up the registers that trampoline.S's sret will use
   // to get to user space.
