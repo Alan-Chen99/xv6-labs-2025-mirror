0b4a6b885ce1eab987a7ac484738e11c57a0be93
Robert Morris <rtm@csail.mit.edu>
2019-07-25 05:35:03 -0400

    comments for timer setup

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -7,11 +7,11 @@ static inline uint64 r_mhartid() {
 
 // Machine Status Register, mstatus
 
-#define MSTATUS_MPP_MASK (3L << 11)
+#define MSTATUS_MPP_MASK (3L << 11) // previous mode.
 #define MSTATUS_MPP_M (3L << 11)
 #define MSTATUS_MPP_S (1L << 11)
 #define MSTATUS_MPP_U (0L << 11)
-#define MSTATUS_MIE (1L << 3)
+#define MSTATUS_MIE (1L << 3) // machine-mode interrupt enable.
 
 static inline uint64 r_mstatus() {
   uint64 x;
