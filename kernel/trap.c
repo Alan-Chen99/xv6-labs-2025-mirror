310a71abad1f989e82e807824bccc3919c0ac142
Robert Morris <rtm@csail.mit.edu>
2019-07-26 11:09:54 -0400

    ???

--- kernel/trap.c
+++ kernel/trap.c
@@ -172,7 +172,7 @@ int devintr() {
 
     plic_complete(irq);
     return 1;
-  } else if (scause == 0x8000000000000001) {
+  } else if (scause == 0x8000000000000001L) {
     // software interrupt from a machine-mode timer interrupt,
     // forwarded by timervec in kernelvec.S.
 
