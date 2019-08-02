db4016f96838164ec0381d4f60a0f0c2f51a47f0
Robert Morris <rtm@csail.mit.edu>
2019-08-02 11:55:26 -0400

    syscall lab nits

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -110,9 +110,7 @@ void syscall(void) {
 
   num = p->tf->a7;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
-    // printf("%d: syscall %d\n", p->pid, num);
     p->tf->a0 = syscalls[num]();
-    // printf("%d: syscall %d -> %d\n", p->pid, num, p->tf->a0);
   } else {
     printf("%d %s: unknown sys call %d\n", p->pid, p->name, num);
     p->tf->a0 = -1;
