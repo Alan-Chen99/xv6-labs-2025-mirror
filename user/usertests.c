f63ca52b60d1a2d8ef919e59c35e65aedf1e0b10
Frans Kaashoek <kaashoek@mit.edu>
2019-09-20 17:07:41 -0400

    Not much: factor sbrksimple from big sbrktest

--- user/usertests.c
+++ user/usertests.c
@@ -1491,15 +1491,10 @@ void forktest(char *s) {
   }
 }
 
-void sbrktest(char *s) {
-  enum { BIG = 100 * 1024 * 1024, TOOMUCH = 1024 * 1024 * 1024 };
-  int i, fds[2], pids[10], pid, ppid;
-  char *c, *oldbrk, scratch, *a, *b, *lastaddr, *p;
-  uint64 amt;
-  int fd;
-  int n;
-
-  oldbrk = sbrk(0);
+void sbrkbasic(char *s) {
+  enum { TOOMUCH = 1024 * 1024 * 1024 };
+  int i, pid, xstatus;
+  char *c, *a, *b;
 
   // does sbrk() return the expected failure value?
   a = sbrk(TOOMUCH);
@@ -1532,7 +1527,19 @@ void sbrktest(char *s) {
   }
   if (pid == 0)
     exit(0);
-  wait(0);
+  wait(&xstatus);
+  exit(xstatus);
+}
+
+void sbrkmuch(char *s) {
+  enum { BIG = 100 * 1024 * 1024, TOOMUCH = 1024 * 1024 * 1024 };
+  int i, fds[2], pids[10], pid, ppid;
+  char *c, *oldbrk, scratch, *a, *lastaddr, *p;
+  uint64 amt;
+  int fd;
+  int n;
+
+  oldbrk = sbrk(0);
 
   // can one grow address space to something big?
   a = sbrk(0);
@@ -1991,7 +1998,8 @@ int main(int argc, char *argv[]) {
       {bigargtest, "bigargtest"},
       {bigwrite, "bigwrite"},
       {bsstest, "bsstest"},
-      {sbrktest, "sbrktest"},
+      {sbrkbasic, "sbrkbasic"},
+      {sbrkmuch, "sbrkmuch"},
       {validatetest, "validatetest"},
       {stacktest, "stacktest"},
       {opentest, "opentest"},
