f18237c99107c73cd8c40e91cc87f28a22488860
Robert Morris <rtm@csail.mit.edu>
2019-10-16 12:27:08 -0400

    nits

--- kernel/pipe.c
+++ kernel/pipe.c
@@ -77,7 +77,7 @@ int pipewrite(struct pipe *pi, uint64 addr, int n) {
   acquire(&pi->lock);
   for (i = 0; i < n; i++) {
     while (pi->nwrite == pi->nread + PIPESIZE) { // DOC: pipewrite-full
-      if (pi->readopen == 0 || myproc()->killed) {
+      if (pi->readopen == 0 || pr->killed) {
         release(&pi->lock);
         return -1;
       }
@@ -100,7 +100,7 @@ int piperead(struct pipe *pi, uint64 addr, int n) {
 
   acquire(&pi->lock);
   while (pi->nread == pi->nwrite && pi->writeopen) { // DOC: pipe-empty
-    if (myproc()->killed) {
+    if (pr->killed) {
       release(&pi->lock);
       return -1;
     }
