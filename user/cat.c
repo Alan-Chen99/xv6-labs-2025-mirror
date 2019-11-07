101b3e2634f659f880fe73cfdca92896c4dced7e
Robert Morris <rtm@csail.mit.edu>
2019-11-07 09:46:20 -0500

    more grind

--- user/cat.c
+++ user/cat.c
@@ -9,12 +9,12 @@ void cat(int fd) {
 
   while ((n = read(fd, buf, sizeof(buf))) > 0) {
     if (write(1, buf, n) != n) {
-      printf("cat: write error\n");
+      fprintf(2, "cat: write error\n");
       exit(1);
     }
   }
   if (n < 0) {
-    printf("cat: read error\n");
+    fprintf(2, "cat: read error\n");
     exit(1);
   }
 }
@@ -24,12 +24,12 @@ int main(int argc, char *argv[]) {
 
   if (argc <= 1) {
     cat(0);
-    exit(1);
+    exit(0);
   }
 
   for (i = 1; i < argc; i++) {
     if ((fd = open(argv[i], 0)) < 0) {
-      printf("cat: cannot open %s\n", argv[i]);
+      fprintf(2, "cat: cannot open %s\n", argv[i]);
       exit(1);
     }
     cat(fd);
