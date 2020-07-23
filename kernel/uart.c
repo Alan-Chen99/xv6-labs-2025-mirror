8527d02823dcddac161034f31084bf20402c8eaa
Robert Morris <rtm@csail.mit.edu>
2020-07-23 06:27:20 -0400

    defines for UART register bits

--- kernel/uart.c
+++ kernel/uart.c
@@ -22,10 +22,18 @@
 #define RHR 0 // receive holding register (for input bytes)
 #define THR 0 // transmit holding register (for output bytes)
 #define IER 1 // interrupt enable register
+#define IER_TX_ENABLE (1 << 0)
+#define IER_RX_ENABLE (1 << 1)
 #define FCR 2 // FIFO control register
-#define ISR 2 // interrupt status register
-#define LCR 3 // line control register
-#define LSR 5 // line status register
+#define FCR_FIFO_ENABLE (1 << 0)
+#define FCR_FIFO_CLEAR (3 << 1) // clear the content of the two FIFOs
+#define ISR 2                   // interrupt status register
+#define LCR 3                   // line control register
+#define LCR_EIGHT_BITS (3 << 0)
+#define LCR_BAUD_LATCH (1 << 7) // special mode to set baud rate
+#define LSR 5                   // line status register
+#define LSR_RX_READY (1 << 0)   // input is waiting to be read from RHR
+#define LSR_TX_IDLE (1 << 5)    // THR can accept another character to send
 
 #define ReadReg(reg) (*(Reg(reg)))
 #define WriteReg(reg, v) (*(Reg(reg)) = (v))
@@ -44,7 +52,7 @@ void uartinit(void) {
   WriteReg(IER, 0x00);
 
   // special mode to set baud rate.
-  WriteReg(LCR, 0x80);
+  WriteReg(LCR, LCR_BAUD_LATCH);
 
   // LSB for baud rate of 38.4K.
   WriteReg(0, 0x03);
@@ -54,13 +62,13 @@ void uartinit(void) {
 
   // leave set-baud mode,
   // and set word length to 8 bits, no parity.
-  WriteReg(LCR, 0x03);
+  WriteReg(LCR, LCR_EIGHT_BITS);
 
   // reset and enable FIFOs.
-  WriteReg(FCR, 0x07);
+  WriteReg(FCR, FCR_FIFO_ENABLE | FCR_FIFO_CLEAR);
 
   // enable transmit and receive interrupts.
-  WriteReg(IER, 0x02 | 0x01);
+  WriteReg(IER, IER_TX_ENABLE | IER_RX_ENABLE);
 
   initlock(&uart_tx_lock, "uart");
 }
@@ -96,7 +104,7 @@ void uartputc_sync(int c) {
   push_off();
 
   // wait for Transmit Holding Empty to be set in LSR.
-  while ((ReadReg(LSR) & (1 << 5)) == 0)
+  while ((ReadReg(LSR) & LSR_TX_IDLE) == 0)
     ;
   WriteReg(THR, c);
 
@@ -114,7 +122,7 @@ void uartstart() {
       return;
     }
 
-    if ((ReadReg(LSR) & (1 << 5)) == 0) {
+    if ((ReadReg(LSR) & LSR_TX_IDLE) == 0) {
       // the UART transmit holding register is full,
       // so we cannot give it another byte.
       // it will interrupt when it's ready for a new byte.
