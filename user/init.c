f19959fb3c4adbaf647c86c308685fb775abc34d
Frans Kaashoek <kaashoek@mit.edu>
2020-08-18 20:48:53 -0400

    Use the major number defined in file.h.  The minor number is ignored; might as well use 0.

--- user/init.c
+++ user/init.c
@@ -2,6 +2,10 @@
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
+#include "kernel/spinlock.h"
+#include "kernel/sleeplock.h"
+#include "kernel/fs.h"
+#include "kernel/file.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
@@ -11,7 +15,7 @@ int main(void) {
   int pid, wpid;
 
   if (open("console", O_RDWR) < 0) {
-    mknod("console", 1, 1);
+    mknod("console", CONSOLE, 0);
     open("console", O_RDWR);
   }
   dup(0); // stdout
