891598feff5164063d3be8bfde6a89e5530a5666
Robert Morris <rtm@csail.mit.edu>
2020-08-19 12:35:14 -0400

    usertest for exec() out of memory recovery and fix a few exec() bugs

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -403,10 +403,9 @@ uint64 sys_exec(void) {
     }
     argv[i] = kalloc();
     if (argv[i] == 0)
-      panic("sys_exec kalloc");
-    if (fetchstr(uarg, argv[i], PGSIZE) < 0) {
       goto bad;
-    }
+    if (fetchstr(uarg, argv[i], PGSIZE) < 0)
+      goto bad;
   }
 
   int ret = exec(path, argv);
--- user/usertests.c
+++ user/usertests.c
@@ -2340,6 +2340,40 @@ void badarg(char *s) {
   exit(0);
 }
 
+// test the exec() code that cleans up if it runs out
+// of memory. it's really a test that such a condition
+// doesn't cause a panic.
+void execout(char *s) {
+  for (int avail = 0; avail < 15; avail++) {
+    int pid = fork();
+    if (pid < 0) {
+      printf("fork failed\n");
+      exit(1);
+    } else if (pid == 0) {
+      // allocate all of memory.
+      while (1) {
+        uint64 a = (uint64)sbrk(4096);
+        if (a == 0xffffffffffffffffLL)
+          break;
+      }
+
+      // free a few pages, in order to let exec() make some
+      // progress.
+      for (int i = 0; i < avail; i++)
+        sbrk(-4096);
+
+      close(1);
+      char *args[] = {"echo", "x", 0};
+      exec("echo", args);
+      exit(0);
+    } else {
+      wait((int *)0);
+    }
+  }
+
+  exit(0);
+}
+
 //
 // use sbrk() to count how many free physical memory pages there are.
 //
@@ -2403,6 +2437,7 @@ int main(int argc, char *argv[]) {
     void (*f)(char *);
     char *s;
   } tests[] = {
+      {execout, "execout"},
       {copyin, "copyin"},
       {copyout, "copyout"},
       {copyinstr1, "copyinstr1"},
