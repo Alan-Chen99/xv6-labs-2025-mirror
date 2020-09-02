1706380975f4a3ec084d6c3e8333e8973d1228d4
Frans Kaashoek <kaashoek@mit.edu>
2020-09-02 17:17:15 -0400

    wfi to save CPU time on Athena

--- kernel/proc.c
+++ kernel/proc.c
@@ -417,6 +417,7 @@ void scheduler(void) {
     // processes are waiting.
     intr_on();
 
+    int found = 0;
     for (p = proc; p < &proc[NPROC]; p++) {
       acquire(&p->lock);
       if (p->state == RUNNABLE) {
@@ -430,8 +431,13 @@ void scheduler(void) {
         // Process is done running for now.
         // It should have changed its p->state before coming back.
         c->proc = 0;
+        found = 1;
       }
       release(&p->lock);
+    }
+    if (found == 0) {
+      intr_on();
+      asm volatile("wfi");
     }
   }
 }
