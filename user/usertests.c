b56b43d96dd7bce88ea1a6f5320eb087631713f4
Robert Morris <rtm@csail.mit.edu>
2020-10-04 09:21:03 -0400

    avoid deadlock by disk intr acking interrupt first, then processing ring

--- user/usertests.c
+++ user/usertests.c
@@ -1655,6 +1655,7 @@ void bigwrite(char *s) {
 // driver.
 void manywrites(char *s) {
   int nchildren = 4;
+  int howmany = 30; // increase to look for deadlock
 
   for (int ci = 0; ci < nchildren; ci++) {
     int pid = fork();
@@ -1670,7 +1671,7 @@ void manywrites(char *s) {
       name[2] = '\0';
       unlink(name);
 
-      for (int iters = 0; iters < 500000; iters++) {
+      for (int iters = 0; iters < howmany; iters++) {
         for (int i = 0; i < ci + 1; i++) {
           int fd = open(name, O_CREATE | O_RDWR);
           if (fd < 0) {
@@ -1686,8 +1687,6 @@ void manywrites(char *s) {
           close(fd);
         }
         unlink(name);
-        if ((iters % 50) == ci)
-          write(1, ".", 1);
       }
 
       unlink(name);
@@ -2615,7 +2614,7 @@ int main(int argc, char *argv[]) {
     void (*f)(char *);
     char *s;
   } tests[] = {
-      // {manywrites, "manywrites"},
+      {manywrites, "manywrites"},
       {execout, "execout"},
       {copyin, "copyin"},
       {copyout, "copyout"},
