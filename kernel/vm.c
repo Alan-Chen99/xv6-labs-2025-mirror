5bf3b8a31cc9d758cc640b8209ff9dac67121f2c
Frans Kaashoek <kaashoek@mit.edu>
2020-10-05 09:17:47 -0400

    Don't map the CLINT, since it never used in the kernel in supervisor mode

--- kernel/vm.c
+++ kernel/vm.c
@@ -28,9 +28,6 @@ void kvminit() {
   // virtio mmio disk interface
   kvmmap(VIRTIO0, VIRTIO0, PGSIZE, PTE_R | PTE_W);
 
-  // CLINT
-  kvmmap(CLINT, CLINT, 0x10000, PTE_R | PTE_W);
-
   // PLIC
   kvmmap(PLIC, PLIC, 0x400000, PTE_R | PTE_W);
 
