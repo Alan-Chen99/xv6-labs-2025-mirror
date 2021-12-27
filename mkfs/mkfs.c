e30d06720a2e15253f3219cf6e68222979005084
mrm <cmpl.error@gmail.com>
2021-12-28 07:06:14 +0900

    fix comment in mkfs.c

--- mkfs/mkfs.c
+++ mkfs/mkfs.c
@@ -46,7 +46,7 @@ uint ialloc(ushort type);
 void iappend(uint inum, void *p, int n);
 void die(const char *);
 
-// convert to intel byte order
+// convert to riscv byte order
 ushort xshort(ushort x) {
   ushort y;
   uchar *a = (uchar *)&y;
