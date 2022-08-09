0ef61a48e23b34f219161222c18d40249de14ff4
Robert Morris <rtm@csail.mit.edu>
2022-08-09 15:11:25 -0400

    nothing much

--- kernel/fs.c
+++ kernel/fs.c
@@ -100,8 +100,8 @@ static void bfree(int dev, uint b) {
 // its size, the number of links referring to it, and the
 // list of blocks holding the file's content.
 //
-// The inodes are laid out sequentially on disk at
-// sb.startinode. Each inode has a number, indicating its
+// The inodes are laid out sequentially on disk at block
+// sb.inodestart. Each inode has a number, indicating its
 // position on the disk.
 //
 // The kernel keeps a table of in-use inodes in memory
