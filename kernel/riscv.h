e603570c92ff7ebca0758513bde5ef2257411692
Robert Morris <rtm@csail.mit.edu>
2022-08-09 11:44:02 -0400

    adopt github PR98 (No need to store TRAPFRAME in sscratch register)

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -1,3 +1,5 @@
+#ifndef __ASSEMBLER__
+
 // which hart (core) is this?
 static inline uint64 r_mhartid() {
   uint64 x;
@@ -239,6 +241,11 @@ static inline void sfence_vma() {
   asm volatile("sfence.vma zero, zero");
 }
 
+typedef uint64 pte_t;
+typedef uint64 *pagetable_t; // 512 PTEs
+
+#endif // __ASSEMBLER__
+
 #define PGSIZE 4096 // bytes per page
 #define PGSHIFT 12  // bits of offset within a page
 
@@ -268,6 +275,3 @@ static inline void sfence_vma() {
 // Sv39, to avoid having to sign-extend virtual addresses
 // that have the high bit set.
 #define MAXVA (1L << (9 + 9 + 9 + 12 - 1))
-
-typedef uint64 pte_t;
-typedef uint64 *pagetable_t; // 512 PTEs
