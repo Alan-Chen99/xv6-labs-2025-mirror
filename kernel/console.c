b83828f65ea1d0c489a5855e4d91496354d6e6d4
Frans Kaashoek <kaashoek@mit.edu>
2022-08-12 12:59:33 -0400

    Cosmetic change (thanks Harry Porter)

--- kernel/console.c
+++ kernel/console.c
@@ -159,7 +159,7 @@ void consoleintr(int c) {
       // store for consumption by consoleread().
       cons.buf[cons.e++ % INPUT_BUF_SIZE] = c;
 
-      if (c == '\n' || c == C('D') || cons.e == cons.r + INPUT_BUF_SIZE) {
+      if (c == '\n' || c == C('D') || cons.e - cons.r == INPUT_BUF_SIZE) {
         // wake up consoleread() if a whole line (or end-of-file)
         // has arrived.
         cons.w = cons.e;
