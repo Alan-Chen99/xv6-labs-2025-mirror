e59ecc04f8709d51fc8e83b3f45287dc59c3ac8b
Frans Kaashoek <kaashoek@mit.edu>
2022-08-24 08:57:56 -0400

    Help students read this C syntax

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -64,6 +64,7 @@ int argstr(int n, char *buf, int max) {
   return fetchstr(addr, buf, max);
 }
 
+// Prototypes for the functions that handle system calls.
 extern uint64 sys_fork(void);
 extern uint64 sys_exit(void);
 extern uint64 sys_wait(void);
@@ -86,6 +87,8 @@ extern uint64 sys_link(void);
 extern uint64 sys_mkdir(void);
 extern uint64 sys_close(void);
 
+// An array mapping syscall numbers from syscall.h
+// to the function that handles the system call.
 static uint64 (*syscalls[])(void) = {
     [SYS_fork] sys_fork,   [SYS_exit] sys_exit,     [SYS_wait] sys_wait,
     [SYS_pipe] sys_pipe,   [SYS_read] sys_read,     [SYS_kill] sys_kill,
@@ -102,6 +105,8 @@ void syscall(void) {
 
   num = p->trapframe->a7;
   if (num > 0 && num < NELEM(syscalls) && syscalls[num]) {
+    // Use num to lookup the system call function for num, call it,
+    // and store its return value in p->trapframe->a0
     p->trapframe->a0 = syscalls[num]();
   } else {
     printf("%d %s: unknown sys call %d\n", p->pid, p->name, num);
