7b9a95d3d6dc4ce9349be021c168be09feba4c22
Frans Kaashoek <kaashoek@mit.edu>
2024-07-26 20:10:08 -0400

    x

--- kernel/proc.c
+++ kernel/proc.c
@@ -436,6 +436,7 @@ void scheduler(void) {
       release(&p->lock);
     }
     if (found == 0) {
+      // nothing to run; stop running on this core until an interrupt.
       intr_on();
       asm volatile("wfi");
     }
