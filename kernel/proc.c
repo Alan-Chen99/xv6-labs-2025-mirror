6b22c0a90896189746ece9e3cfd5769dbd3877e4
Ivy Wu <ivy.wu.2003@gmail.com>
2024-11-13 16:12:43 -0500

    Remove wfi for fs lab

--- kernel/proc.c
+++ kernel/proc.c
@@ -440,7 +440,9 @@ void scheduler(void) {
     }
     if (found == 0) {
       // nothing to run; stop running on this core until an interrupt.
+#ifndef LAB_FS
       asm volatile("wfi");
+#endif
     }
   }
 }
