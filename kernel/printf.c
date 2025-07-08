7385850911e6a04133255e983f01991b8ae54454
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2025-07-09 00:17:43 +0330

    Fixed buffer overflow in printint (#299)
    There is a buffer overflow in printint function which can be triggered if numbers with more than 16 digits are written to the terminal. Numbers 18446744073709551615 and -9223372036854775808 both have 20 digits for instance. To this extent, I increased the buffer size to 20 characters which can fit base 10 of all 64 bit numbers (negative or positive).

--- kernel/printf.c
+++ kernel/printf.c
@@ -26,7 +26,7 @@ static struct {
 static char digits[] = "0123456789abcdef";
 
 static void printint(long long xx, int base, int sign) {
-  char buf[16];
+  char buf[20];
   int i;
   unsigned long long x;
 
