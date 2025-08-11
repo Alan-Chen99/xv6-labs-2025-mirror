830f5288e0f8d36915657f3d3d3f9d8138e90b50
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-08-11 15:39:45 -0700

    more precise tracking of block space in log

--- kernel/log.c
+++ kernel/log.c
@@ -34,13 +34,12 @@
 // and to keep track in memory of logged block# before commit.
 struct logheader {
   int n;
-  int block[LOGSIZE];
+  int block[LOGBLOCKS];
 };
 
 struct log {
   struct spinlock lock;
   int start;
-  int size;
   int outstanding; // how many FS sys calls are executing.
   int committing;  // in commit(), please wait.
   int dev;
@@ -57,7 +56,6 @@ void initlog(int dev, struct superblock *sb) {
 
   initlock(&log.lock, "log");
   log.start = sb->logstart;
-  log.size = sb->nlog;
   log.dev = dev;
   recover_from_log();
 }
@@ -118,7 +116,7 @@ void begin_op(void) {
   while (1) {
     if (log.committing) {
       sleep(&log, &log.lock);
-    } else if (log.lh.n + (log.outstanding + 1) * MAXOPBLOCKS > LOGSIZE) {
+    } else if (log.lh.n + (log.outstanding + 1) * MAXOPBLOCKS > LOGBLOCKS) {
       // this op might exhaust log space; wait for commit.
       sleep(&log, &log.lock);
     } else {
@@ -197,7 +195,7 @@ void log_write(struct buf *b) {
   int i;
 
   acquire(&log.lock);
-  if (log.lh.n >= LOGSIZE || log.lh.n >= log.size - 1)
+  if (log.lh.n >= LOGBLOCKS)
     panic("too big a transaction");
   if (log.outstanding < 1)
     panic("log_write outside of trans");
