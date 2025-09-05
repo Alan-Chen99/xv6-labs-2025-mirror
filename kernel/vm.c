0f2fd4dbf47ded6b6916b29255fdf462bf94092e
Ben Holmes <bcwh@csail.mit.edu>
2025-09-05 14:37:14 -0400

    add ifndef blocks for attack lab

--- kernel/vm.c
+++ kernel/vm.c
@@ -209,7 +209,9 @@ uint64 uvmalloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz, int xperm) {
       uvmdealloc(pagetable, a, oldsz);
       return 0;
     }
+#ifndef LAB_SYSCALL
     memset(mem, 0, PGSIZE);
+#endif
     if (mappages(pagetable, a, PGSIZE, (uint64)mem, PTE_R | PTE_U | xperm) !=
         0) {
       kfree(mem);
