6f26a26c79bed7690d0cd6fba9902772673d15f6
Nickolai Zeldovich <nickolai@csail.mit.edu>
2025-09-08 14:43:08 -0400

    fix start() args

--- user/ulib.c
+++ user/ulib.c
@@ -8,10 +8,10 @@
 //
 // wrapper so that it's OK if main() does not call exit().
 //
-void start() {
+void start(int argc, char **argv) {
   int r;
-  extern int main();
-  r = main();
+  extern int main(int argc, char **argv);
+  r = main(argc, argv);
   exit(r);
 }
 
